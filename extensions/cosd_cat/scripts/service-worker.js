be0569beffacc6e32dc4a3e573d13c14bdafbc74
christovic <dan.christovic@octopusenergy.group>
2025-09-04 23:30:54 +0100

    add basic proxy functionality to extension

--- extensions/cosd_cat/scripts/service-worker.js
+++ extensions/cosd_cat/scripts/service-worker.js
@@ -1,4 +1,65 @@
 const tabErrors = {};
+const wantedResourceTypes = [
+    "main_frame",
+    "sub_frame",
+    "stylesheet",
+    "script",
+    "image",
+    "font",
+    "object",
+    "xmlhttprequest",
+    "ping",
+    "csp_report",
+    "media",
+    "websocket",
+    "webtransport",
+    "webbundle",
+    "other",
+];
+
+async function setDeclarativeNetworkRequest() {
+    chrome.storage.local.get().then((result) => {
+        if (!result.launchUrls) {
+            console.log("launchUrls not set yet, waiting and trying again.")
+            return;
+        } else if (!result.upstreamUrl) {
+            console.log("upstreamUrl not set, not proxying.")
+            clearInterval(intervalId)
+            return;
+        }
+
+        if (result.upstreamUrl && result.authHeaderKey && result.authHeaderValue) {
+            let headerRule = {
+                id: 1,
+                priority: 1,
+                action: {
+                    type: "modifyHeaders",
+                    requestHeaders: [
+                        {
+                            header: result.authHeaderKey,
+                            operation: "set",
+                            value: result.authHeaderValue,
+                        },
+                    ],
+                },
+                condition: {
+                    urlFilter: result.upstreamUrl,
+                    resourceTypes: wantedResourceTypes,
+                },
+            };
+            chrome.declarativeNetRequest.getSessionRules().then((rules) => {
+                chrome.declarativeNetRequest.updateSessionRules({
+                    removeRuleIds: rules.map((rule) => rule.id),
+                    addRules: [headerRule],
+                });
+            });
+        }
+        clearInterval(intervalId);
+    });
+}
+
+let intervalId = setInterval(setDeclarativeNetworkRequest, 1000)
+setDeclarativeNetworkRequest();
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request == "errorDetails") {
@@ -14,6 +75,7 @@ async function onErrorOccurred(details) {
     if (
         !details.parentDocumentId &&
         !details.documentId &&
+        !details.documentUrl &&
         details.documentLifecycle == "active" &&
         details.error != "net::ERR_ABORTED"
     ) {
--- src/server.js
+++ src/server.js
@@ -22,6 +22,9 @@ const DISPLAY_SCALE = process.env.DISPLAY_SCALE || "1.0";
 const LAUNCH_URLS = (
     process.env.LAUNCH_URL || "file:///home/chromium/index.html"
 ).split(",");
+const AUTH_HEADER_KEY = process.env.AUTH_HEADER_KEY;
+const AUTH_HEADER_VALUE = process.env.AUTH_HEADER_VALUE;
+const UPSTREAM_URL = process.env.UPSTREAM_URL;
 const REFRESH_SCHEDULE = process.env.REFRESH_SCHEDULE || 0;
 const ROTATE_SCHEDULE = process.env.ROTATE_SCHEDULE || 0;
 const RELOAD_ON_ERROR = process.env.RELOAD_ON_ERROR || 1;
@@ -259,6 +262,8 @@ async function setExtensionStorage() {
         fontFamily: OSD_FONT_FAMILY,
         showDeviceTag: SHOW_DEVICE_TAG,
         reloadOnErrorTimer: RELOAD_ON_ERROR_TIMER,
+        authHeaderKey: AUTH_HEADER_KEY,
+        authHeaderValue: AUTH_HEADER_VALUE,
     };
     const jsonData = JSON.stringify(extensionConfig);
 
